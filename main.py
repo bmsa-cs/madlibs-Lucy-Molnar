021a214ad45485c0803bb67eef259f6870a9da0d
Lucy-Molnar <74046470+Lucy-Molnar@users.noreply.github.com>
2020-11-10 09:53:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author:Lucy Molnar 
+Period/Core:5
 
 
 """
+print("Let's play Silly Sentences!")
+print("welcome " + "to Lucy's Mad Libs Program")
+name = input("enter a boy number ones name:")
+print("decided he was going to take a trip to the")
+place = input("enter a place:")
+print("he was super exited an couldnt wait but they didn't wanna go alone so he called his friend ")
+adjective = input("enter an adjective:")
+name = input("enter boy number twos name:")
+print("his friend picked up the phone and after a bit of talking they both said they would meet up at the place.")
+name = input("enter boy number ones name:")
+print("arived at the destination and waited for his friend")
+adjective = input("same adjective as before:")
+name = input("enter boy number twos name:")
+print("soon pulled into the parking lot both friends were super exited they ended up getting a ton of food while they were hanging out some of the things they ate were")
+food = input("pick a food:")
+food = input("pick anouther food:")
+print("and they also ate quite a few burgers once the day was over they had to head home so they")
+adverb = input("pick an adverb:")
+verb = input("pick a verb:")
+print("back to there cars and headed home the end")
+name = input("Lucy's")
+project = input("program")
+print(f"My name is {name} and I hope you like my {project}.")
